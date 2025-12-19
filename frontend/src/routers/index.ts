e4aa712fc55a194004b42e850561f131bceb121d
Kaftow <166228791+Kaftow@users.noreply.github.com>
2025-12-19 15:33:11 +0900

    feat(frontend): implement chunk configer
    * feat(frontend): implement storage layer for chunking config
    
    * feat(frontend): make chunk configer

--- frontend/src/routers/index.ts
+++ frontend/src/routers/index.ts
@@ -1,16 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import FileLoader from "@/views/FileLoader.vue";
 import TextEditor from "@/views/TextEditor.vue";
+import TextChunker from "@/views/TextChunker.vue";
 
 export const stepRouteMap: Record<string, string> = {
   fileLoader: "/file-loader",
   textEditor: "/text-editor",
+  textChunker: "/text-chunker",
 };
 
 const routes = [
   { path: "/", redirect: "/file-loader" },
   { path: "/file-loader", name: "fileLoader", component: FileLoader },
   { path: "/text-editor", name: "textEditor", component: TextEditor },
+  { path: "/text-chunker", name: "textChunker", component: TextChunker },
 ];
 
 const router = createRouter({
